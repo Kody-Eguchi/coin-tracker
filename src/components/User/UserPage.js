58e57539a33a4a87e8808c92d2559794fa8f31b0
Koji Eguchi <koji.eguchi@hotmail.com>
2023-09-07 20:26:10 -0400

    Try to add a function to read cross-domain cookie

--- src/components/User/UserPage.js
+++ src/components/User/UserPage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./Login";
+import UserProfile from "./UserProfile";
+import Cookies from "js-cookie";
+import api from "../../config/axios-config";
 
 function UserPage() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    console.log(token, "🔑");
+    if (token) {
+      api
+        .get("/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setUser(response.data);
+        })
+        .catch((error) => {
+          console.error("Error retrieving user information", error);
+        });
+    }
+  }, []);
+
   return (
     <div>
       <h1>User Page</h1>
-      <Login />
+      {user ? <UserProfile user={user} /> : <Login />}
     </div>
   );
 }
