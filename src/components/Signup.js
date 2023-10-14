669198ceb1e23b089bfe1152f18015e3305b58a4
Koji Eguchi <koji.eguchi@hotmail.com>
2023-10-14 16:47:32 -0400

    Render user friendly message when sign up was successfully completed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import api from "../config/axios-config";
 import Cookies from "js-cookie";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
@@ -8,6 +9,8 @@ const SignUp = () => {
     email: "",
     password: "",
   });
+
+  const [SignUpStatus, setSignUpStatus] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,8 +32,8 @@ const SignUp = () => {
       });
       if (response.status === 201) {
         console.log("New user created successfully");
-        // CREATE A TOKEN
-        // LOGIN USER OR NAVIATE USER TO LOGIN PAGE
+
+        setSignUpStatus(true);
       } else {
         console.error("Failed to create an account");
       }
@@ -42,36 +45,43 @@ const SignUp = () => {
   return (
     <div>
       <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
+      {SignUpStatus ? (
         <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
+          <h2>New User Account Is Created Successfully!</h2>
+          <Link to="/login">Go to Login</Link>
         </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      )}
     </div>
   );
 };
