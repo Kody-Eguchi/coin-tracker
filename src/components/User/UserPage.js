7c4dcb733a34c3a2c9ca17c97318f42199160b18
Koji Eguchi <koji.eguchi@hotmail.com>
2023-10-19 19:19:30 -0400

    Add minimal styling to UserPage and UserProfile component

--- src/components/User/UserPage.js
+++ src/components/User/UserPage.js
@@ -7,15 +7,24 @@ function UserPage() {
   const { isLoggedIn, user, fetchUserData } = useAuth();
   useEffect(() => {
     fetchUserData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div>
-      <h1>User Page</h1>
-      {isLoggedIn && user ? (
-        <UserProfile />
-      ) : (
-        <Link to="/login">Go to Login</Link>
-      )}
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+          {isLoggedIn && user ? (
+            <UserProfile />
+          ) : (
+            <div className="text-center mt-4">
+              <p>You are not logged in.</p>
+              <Link to="/login" className="btn btn-primary">
+                Go to Login
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/User/UserProfile.js
+++ src/components/User/UserProfile.js
@@ -4,10 +4,20 @@ import { useAuth } from "../../hooks/useAuth";
 function UserProfile() {
   const { user } = useAuth();
   return (
-    <div>
-      <h1>User Profile!!!</h1>
-      <p>{user.username}</p>
-      <p>{user.email}</p>
+    <div className="text-center">
+      <div className="card bg-light" style={{ maxWidth: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title text-success">User Profile</h5>
+          <div className="mb-3">
+            <h4>Username:</h4>
+            <p className="user-info">{user.username}</p>
+          </div>
+          <div className="mb-3">
+            <h4>Email:</h4>
+            <p className="user-info">{user.email}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
